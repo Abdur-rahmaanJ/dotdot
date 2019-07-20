518f73de40b82e101f7990324b673ac8d399f77c
Abdur-RahmaanJ <arj.python@gmail.com>
2019-07-20 14:06:02 +0400

    oopfied source code

--- dotdot/dotdot.py
+++ dotdot/dotdot.py
@@ -1,32 +1,17 @@
-
 import copy 
 
-source = '''
 
-x = 1
-y = 5
-x-y-z = 6
-col-z = berry
+class Utils:
+    def __init__(self):
+        pass
 
-@f{
-    border: 10px solid black;
-    border-radius: 50%;
-}
+    @staticmethod
+    def read_source(path: str) -> str:
+        source = ''
+        with open(path, encoding='utf8') as f:
+            source = f.read()
+        return source
 
-.add{
-    color:white;
-    background-color: rgb(3, 4, 5);
-}
-
-#ad{
-    color: rgb(x, 4, 5);
-}
-
-.a div a{
-    color: ..col-z;
-    @f
-}
-'''
 
 
 class SYM:
@@ -67,132 +52,134 @@ class Lexer:
                     if self.lexeme != '':
                         self.lexemes.append(self.lexeme)
                         self.lexeme = ''
+        self.lexemes.append('') # appending last element to dump values well
         return self.lexemes
 
 
+class Tree:
+    def __init__(self, lexemes: list):
+        self.tree = {}
+        self.memory = {}
+        self.lexemes = lexemes
+
+        self.block_ongoing = False
+        self.attribute_ongoing = False
+        self.value_ongoing = False
+        self.statement_ongoing = False
+
+        self.id_string = ''
+        self.last_id_string = ''
+        self.last_attribute = ''
+
+        self.current_attribute = ''
+        self.current_value = ''
+
+        self.len_lexemes = len(lexemes)
+
+    def gen(self) -> None:
+        for i, lex in enumerate(self.lexemes):
+            if i+1 < self.len_lexemes:
+                next_lexeme = self.lexemes[i+1]
+            prev_lexeme = self.lexemes[i-1]
+
+            if lex == SYM.LEFT_BRACE:
+                self.block_ongoing = True
+                self.attribute_ongoing = True
+                continue
+            elif lex == SYM.RIGHT_BRACE:
+                self.block_ongoing = False
+                self.statement_ongoing = False
+                self.attribute_ongoing = False
+                self.alue_ongoing = False
+                continue
+            if lex == SYM.COLON:
+                self.value_ongoing = True
+                self.attribute_ongoing = False
+                continue
+            elif lex == SYM.SEMI_COLON:
+                self.value_ongoing = False
+                self.statement_ongoing = False
+                self.attribute_ongoing = True
+                continue
+
+            if lex == SYM.EQUAL:
+                self.memory[prev_lexeme] = next_lexeme
+                continue
+            elif next_lexeme == SYM.EQUAL or prev_lexeme == SYM.EQUAL:
+                continue
+
+            if not self.block_ongoing:
+                self.id_string += lex + ' '
+            elif self.block_ongoing:
+                if self.id_string:
+                    self.tree[self.id_string.strip()] = {}
+                    self.last_id_string = self.id_string.strip()
+                    self.id_string = ''
+
+            if self.attribute_ongoing:
+                self.current_attribute += lex
+            elif not self.attribute_ongoing:
+                if self.current_attribute:
+                    self.tree[self.last_id_string][self.current_attribute] = ''
+                    self.last_attribute = self.current_attribute
+                    self.current_attribute = ''
+
+            if self.value_ongoing:
+                self.current_value += lex + ' '
+            elif not self.value_ongoing:
+                if self.current_value:
+                    self.tree[self.last_id_string][self.last_attribute] = self.current_value.strip()
+                    self.last_value = self.current_value.strip()
+                    self.current_value = ''
+
+    def parseFunc(self, f: str) -> str:
+        v = f.split(SYM.LEFT_ROUND)
+        name = v[0]
+        vals = v[1][:-1].replace(SYM.SPACE, '')
+        values = vals.split(SYM.COMMA)
+        for i, v in enumerate(values):
+            if not v.isnumeric():
+                values[i] = self.memory[v]
+        return '{}({})'.format(name.strip(), ','.join(values))
+
+    def parse(self) -> None:
+        self.ntree = copy.deepcopy(self.tree)
+
+        for block_name in self.ntree:
+            properties = self.ntree[block_name]
+            if block_name[0] == SYM.AT:
+                continue
+            for element in properties:
+                value = properties[element]
+                if SYM.LEFT_ROUND in value:
+                    self.tree[block_name][element] = self.parseFunc(value)
+                if SYM.DOT in value:
+                    self.tree[block_name][element] = self.memory[value.strip(SYM.DOT)]
+                if SYM.AT in element:
+                    del self.tree[block_name][element]
+                    self.tree[block_name].update(self.tree[element])
+
+    def output(self, css_filename: str, mode="compile") -> None:
+        if mode == 'compile':
+            with open(css_filename, 'w+', encoding='utf8') as f:
+                for key in self.tree:
+                    if key[0] == SYM.AT:
+                        continue
+                    f.write('{}{{{}'.format(key, SYM.NEW_LINE))
+                    for elem in self.tree[key]:
+                        f.write('{}{}: {};{}'.format(SYM.TAB, elem, 
+                            self.tree[key][elem], SYM.NEW_LINE))
+                    f.write('}}{}'.format(SYM.NEW_LINE*2))
+
+
+source = Utils.read_source('source.dot')
 v = Lexer(source, KEYWORDS)
 lexemes = v.get_lexemes()
 # print(lexemes)
-lexemes.append('') # appending an unused last element to properly dump all values
 
-tree = {}
-memory = {}
-
-block_ongoing = False
-attribute_ongoing = False
-value_ongoing = False
-statement_ongoing = False
-
-id_string = ''
-last_id_string = ''
-last_attribute = ''
-
-current_attribute = ''
-current_value = ''
-
-len_lexemes = len(lexemes)
-
-for i, lex in enumerate(lexemes):
-
-    if i+1 < len_lexemes:
-        next_lexeme = lexemes[i+1]
-    prev_lexeme = lexemes[i-1]
-
-    if lex == SYM.LEFT_BRACE:
-        block_ongoing = True
-        attribute_ongoing = True
-        continue
-    elif lex == SYM.RIGHT_BRACE:
-        block_ongoing = False
-        statement_ongoing = False
-        attribute_ongoing = False
-        value_ongoing = False
-        continue
-    if lex == SYM.COLON:
-        value_ongoing = True
-        attribute_ongoing = False
-        continue
-    elif lex == SYM.SEMI_COLON:
-        value_ongoing = False
-        statement_ongoing = False
-        attribute_ongoing = True
-        continue
-
-    if lex == SYM.EQUAL:
-        memory[prev_lexeme] = next_lexeme
-        continue
-    elif next_lexeme == SYM.EQUAL or prev_lexeme == SYM.EQUAL:
-        continue
-
-    if not block_ongoing:
-        id_string += lex + ' '
-    elif block_ongoing:
-        if id_string:
-            tree[id_string.strip()] = {}
-            last_id_string = id_string.strip()
-            id_string = ''
-
-    if attribute_ongoing:
-        current_attribute += lex
-    elif not attribute_ongoing:
-        if current_attribute:
-            tree[last_id_string][current_attribute] = ''
-            last_attribute = current_attribute
-            current_attribute = ''
-
-    if value_ongoing:
-        current_value += lex + ' '
-    elif not value_ongoing:
-        if current_value:
-            tree[last_id_string][last_attribute] = current_value.strip()
-            last_value = current_value.strip()
-            current_value = ''
-
-    #print(f'''
-    #    i:{i}, lex: {lex}
-    #    current_id: {id_string}, last: {last_id_string}
-    #    current_attribute: {current_attribute}
-    #    current_value: {current_value}''')
-
-    
-print(tree)
-# print(memory)
-
-def parseFunc(f):
-    v = f.split(SYM.LEFT_ROUND)
-    name = v[0]
-    vals = v[1][:-1].replace(SYM.SPACE, '')
-    values = vals.split(SYM.COMMA)
-    for i, v in enumerate(values):
-        if not v.isnumeric():
-            values[i] = memory[v]
-    return '{}({})'.format(name.strip(), ','.join(values))
-
-ntree = copy.deepcopy(tree)
-
-for block_name in ntree:
-    properties = ntree[block_name]
-    if block_name[0] == SYM.AT:
-        continue
-    for element in properties:
-        value = properties[element]
-        if SYM.LEFT_ROUND in value:
-            tree[block_name][element] = parseFunc(value)
-        if SYM.DOT in value:
-            tree[block_name][element] = memory[value.strip(SYM.DOT)]
-        if SYM.AT in element:
-            del tree[block_name][element]
-            tree[block_name].update(tree[element])
-
-
-# print(parseFunc('rgb (x-y-z , 4 , 5 )'))
-# print(tree)
-
-# display
-for key in tree:
-    if key[0] == SYM.AT:
-        continue
-    print(key, '{', sep='')
-    for elem in tree[key]:
-        print('{}{}: {};'.format(SYM.TAB, elem, tree[key][elem]))
-    print('}\n')
+tree = Tree(lexemes)
+tree.gen()
+print(tree.tree)
+tree.parse()
+print(tree.tree)
+tree.output('compiled.css')
